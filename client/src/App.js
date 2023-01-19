c748d14c30dbf857646a16927396f6c09b8afe89
Pablo Fuentes Tudela <pablo.fuentes.tudela@outlook.com>
2023-01-18 20:17:21 -0500

    commented out all nonfunc code + add label to create location form

--- client/src/App.js
+++ client/src/App.js
@@ -6,10 +6,13 @@ import {useState, useEffect} from 'react';
 function App() {
 
   const [loggedInUser, setLoggedInUser] = useState(null)
-  console.log(loggedInUser)
+  // console.log(loggedInUser)
 
   const [loggedInUserLocations, setLoggedInUserLocations] = useState([])
-  console.log(loggedInUserLocations)
+  // console.log(loggedInUserLocations)
+
+  // const [allTheCategories, setAllTheCategories] = useState([])
+  // // console.log(allTheCategories)
 
   useEffect(
     ()=>{
@@ -22,12 +25,24 @@ function App() {
         setLoggedInUserLocations(userAlreadyLoggedIn.locations)
       })
 
+      // fetch("/categories") 
+      // .then(r => r.json())
+      // .then(setAllTheCategories) // setAllTheCategories
+
       // fetch("/locations")  // may  need to change this to room/location
       // .then(r => r.json)
       // .then(console.log)
     }
     ,[]
   )
+
+  
+  // fetch("/categories") 
+  // .then(res=>res.json())
+  // .then(console.log)
+  // console.log(allTheCategories)
+  
+  // console.log(allTheCategories)
 
 
   const [userLogin, setUserLogin] = useState(
@@ -36,12 +51,12 @@ function App() {
       password: ""
     }
   )
-  console.log(userLogin)
+  // console.log(userLogin)
 
 
 
   const handleChange=(e)=>{
-    console.log(e)
+    // console.log(e)
     setUserLogin({...userLogin, [e.target.name]: e.target.value})
   }
 
@@ -57,7 +72,7 @@ function App() {
     })
     .then(response => response.json())
     .then(loggedIn => {
-      console.log(loggedIn)
+      // console.log(loggedIn)
       setLoggedInUser(loggedIn)
       setLoggedInUserLocations(loggedIn.locations)
     })
@@ -76,7 +91,7 @@ function App() {
   const [newLocation, updateNewLocationInfo] = useState({
     name: ""
   })
-  console.log(newLocation)
+  // console.log(newLocation)
   
   const changeHandlerNewLocationName=(e)=>{
     updateNewLocationInfo({...newLocation, name: e.target.value})
@@ -111,6 +126,26 @@ function App() {
     // .then(console.log)
   }
 
+  ///////// dropdown join model post request methods
+
+  // const [newUserItem, updateNewUserItem] = useState({
+  //   name: "",
+  //   description: "",
+  //   image_url: "",
+  //   location_in_room: "",
+  //   user_id: "",
+  //   location_id: ""
+  // })
+
+  // const handleSubmitForItemCreate =(e)=>{
+  //   e.preventDefault()
+
+
+  // }
+
+  // const changeHandler
+
+
   return (
     <div className="App">
       <header className="App-header">
@@ -124,6 +159,28 @@ function App() {
             {loggedInUserLocations.map((eachLocation)=>{
               return(<h4>{eachLocation.name}</h4>)
             })}
+            {/* <form onSubmit={()=>{}}>
+              <select>
+                <option value={0}>Select Location</option>
+                {
+                loggedInUserLocations.map(( eachLocation)=>{
+                  return(<option value={eachLocation.id}>{eachLocation.name}</option>)
+                })
+                }
+              </select>
+                <input onChange={()=>{}}/>
+              <select>
+                <option value={0}>Select Category</option>
+                {
+                  allTheCategories.map((eachCategory)=>{
+                    return(<option value={eachCategory.id}>{eachCategory.name}</option>)
+                  })
+                }
+              </select> 
+              <br/>
+              <input type="submit"/>
+            </form> */}
+
           </>
         )
         : (<><h1>Welcome! login!!</h1>
@@ -134,7 +191,9 @@ function App() {
           <input type="submit" value="login" />
         </form></>)}
 
+            {/* cant seem to move this without breaking */}
         <form onSubmit={submitHandlerNewLocation}>
+          <label>Add Location</label>
           <input onChange={changeHandlerNewLocationName} name="name" value={newLocation.name}/>
           <input type="submit"/>
         </form>
